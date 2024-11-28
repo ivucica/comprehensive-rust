3c3fa0be96de6944938d689f44d27c1b48f11b53
Ivan Vučica <ivan@vucica.net>
2024-11-28 16:02:52 +0000

    Transpose incl nonsquare transpose.

--- transpose/transpose.rs
+++ transpose/transpose.rs
@@ -1,8 +1,14 @@
 // TODO: remove this when you're done with your implementation.
 #![allow(unused_variables, dead_code)]
 
-fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-    unimplemented!()
+fn transpose<const M : usize, const N : usize>(matrix: [[i32; M]; N]) -> [[i32; N]; M] {
+    let mut new_rows : [[i32; N]; M] = [[0; N]; M];
+    for new_col_id in 0..matrix.len() {
+        for new_row_id in 0..matrix[new_col_id].len() {
+            new_rows[new_row_id][new_col_id] = matrix[new_col_id][new_row_id];
+        }
+    }
+    new_rows
 }
 
 #[test]
@@ -19,6 +25,25 @@ fn test_transpose() {
             [101, 201, 301], //
             [102, 202, 302],
             [103, 203, 303],
+        ]
+    );
+}
+
+#[test]
+fn test_transpose_nonsquare() {
+    let matrix = [
+        [101, 102, 103, 104], //
+        [201, 202, 203, 204],
+        [301, 302, 303, 304],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+            [104, 204, 304],
         ]
     );
 }
