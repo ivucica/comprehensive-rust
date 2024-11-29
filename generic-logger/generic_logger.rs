0f1b22437e6ecf25079dfbd86f0e1cff7ecbdcce
Ivan Vučica <ivan@vucica.net>
2024-11-29 12:40:55 +0000

    Boxed version

--- generic-logger/generic_logger.rs
+++ generic-logger/generic_logger.rs
@@ -11,10 +11,21 @@ impl Logger for StdoutLogger {
     }
 }
 
-// TODO: Define and implement `VerbosityFilter`.
+struct VerbosityFilter {
+    max_verbosity: u8,
+    inner: Box<dyn Logger>,
+}
+impl Logger for VerbosityFilter {
+    fn log(&self, verbosity: u8, message: &str) {
+        if verbosity <= self.max_verbosity {
+            self.inner.log(verbosity, message)
+        }
+    }
+}
+
 
 fn main() {
-    let logger = VerbosityFilter { max_verbosity: 3, inner: StdoutLogger };
+    let logger = VerbosityFilter { max_verbosity: 3, inner: Box::new(StdoutLogger) };
     logger.log(5, "FYI");
     logger.log(2, "Uhoh");
 }
